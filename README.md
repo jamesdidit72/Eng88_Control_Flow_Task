7b8cce600180044364da8db5c88937b6bc5c1107
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-20 14:28:57 +0100

    second iteration of task

--- README.md
+++ README.md
@@ -51,3 +51,35 @@ while leave:
     else:
         print('Please enter a valid value')
 ```
+## second iteration
+```python
+film_rating = [
+    'universal -> everyone can watch.',  # film_rating[0]
+    'pg --> General viewing, but some scenes may be unsuitable for young children.',  # film_rating[1]
+    '12 --> Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.',  # film_rating[2]
+    '15 --> No one younger than 15 may see a 15 film in a cinema.',  # film_rating[3]
+    '18 --> No one younger than 18 may see an 18 film in a cinema.'  # film_rating[4]
+]
+
+leave = True
+while leave:
+    rating = input(
+        'What rating would you like to learn about? Type exit to leave.  ')  # asks user for an input on film rating
+    rating = rating.lower()  # sets the string case to lower
+    if rating == 'exit':  # if exit is inputted, then the loop ends
+        print('Thank you for checking out the ratings')
+        leave = False
+    elif rating == 'u':
+        print(film_rating[0])
+    elif rating == 'pg':
+        print(film_rating[1])
+    elif rating == '12':
+        print(film_rating[2])
+    elif rating == '15':
+        print(film_rating[3])
+    elif rating == '18':
+        print(film_rating[4])
+    else:
+        print('Please enter a valid value')
+
+```
--- movie_rating.py
+++ movie_rating.py
@@ -1,28 +1,30 @@
 # Movie ratings
 
 film_rating = [
-    'universal -> everyone can watch.',
-    'pg --> General viewing, but some scenes may be unsuitable for young children.',
-    '12 --> Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.',
-    '15 --> No one younger than 15 may see a 15 film in a cinema.',
-    '18 --> No one younger than 18 may see an 18 film in a cinema.'
+    'universal -> everyone can watch.',  # film_rating[0]
+    'pg --> General viewing, but some scenes may be unsuitable for young children.',  # film_rating[1]
+    '12 --> Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.',  # film_rating[2]
+    '15 --> No one younger than 15 may see a 15 film in a cinema.',  # film_rating[3]
+    '18 --> No one younger than 18 may see an 18 film in a cinema.'  # film_rating[4]
 ]
 
 leave = True
 while leave:
-    rating = input('What rating would you like to learn about? Type exit to leave.  ')
-    if rating.lower() == 'exit':
+    rating = input(
+        'What rating would you like to learn about? Type exit to leave.  ')  # asks user for an input on film rating
+    rating = rating.lower()  # sets the string case to lower
+    if rating == 'exit':  # if exit is inputted, then the loop ends
         print('Thank you for checking out the ratings')
         leave = False
-    elif rating.lower() == 'u':
+    elif rating == 'u':
         print(film_rating[0])
-    elif rating.lower() == 'pg':
+    elif rating == 'pg':
         print(film_rating[1])
-    elif rating.lower() == '12':
+    elif rating == '12':
         print(film_rating[2])
-    elif rating.lower() == '15':
+    elif rating == '15':
         print(film_rating[3])
-    elif rating.lower() == '18':
+    elif rating == '18':
         print(film_rating[4])
     else:
         print('Please enter a valid value')
